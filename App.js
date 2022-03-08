0272aa37bd901b108da59255c9c9ea541bd553fc
RdzJJ <98420262+RdzJJ@users.noreply.github.com>
2022-03-07 21:27:15 -0500

    app_commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// components
+import Navbar from ".components/Navbar";
+import CharacterList from "./components/CharacterList";
+
+function App() {
+  const [characters,setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+  const url ="https://rickandmortyapi.com/api/character";
+
+  const fetchCharacters = (url) => {
+    axios
+      .get(url)
+      .then((data) => {
+        setCharacters(data.data.results);
+        setInfo(data.data.info);
+      })
+      .catch((eror) => {
+        console.log(error);
+      };
+};
+
+  const handleNextPage = () => {
+    fetchCharacters(info.next);
+    window.scrollTo(0, 0);
+  };
+
+  const handlePreviousPage
+export default App;
